f1ffa6709d31c90389e9ccc2766b84d0eb04dcdc
Saikumar Voddepally <dredwing147@gmail.com>
2024-11-29 23:14:03 +0530

    removed project folders

--- Backend/FacultyFeedbackSystem/src/main/java/com/example/demo/FacultyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FacultyRepository extends JpaRepository<FacultyData, Integer> {
-	
-	boolean existsById(int id);
-
-}
